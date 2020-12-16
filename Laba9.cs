6b3e66872a023e0c0293ec285aab690fb437371d
Cumber Cucumber <Wanjawaluj111@gmail.com>
2020-12-16 15:59:43 +0300

    1 commit

---
+++ Laba9.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba9
+{
+    class Laba9
+    {
+        public class User
+        {
+            public delegate void DelegMove(string message);
+            public event DelegMove Move;
+            public delegate void DelegSqueeze(string message);
+            public event DelegSqueeze Squeeze;
+
+            public int Placing { get; set; } // Размежение
+            public int Compression { get; set; } // Сжатие
+            public User(int _placing, int _compression)
+            {
+                Placing = _placing;
+                Compression = _compression;
+            }
+
+            public void FuncMove(int x)
+            {
+                Placing += x;
+                Move.Invoke("Размещение стало равно " + Placing); // Т.к. делегат DelMove принимает строку, то при вызове события надо её передать
+            }
+            public void FuncSqueeze(int y)
+            {
+                int save = Compression;
+                Compression = y;
+                Squeeze.Invoke("Коэффициент сжатия изменился с " + save + " на " + Compression);
+            }
+        }
+
+
+
+        public static void OutputBlueMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue; // Устанавливаем красный цвет символов
+            Console.WriteLine(message);
+            Console.ResetColor(); // Сбрасываем настройки цвета
+        }
+
+
+
+        public delegate void TypeDeleg(string message);
+        public class Type1
+        {
+            public event TypeDeleg TypeEvent;
+            public int typelvl = 1;
+            public void LvlUp() 
+            {
+                TypeEvent.Invoke($"Уровень Type1 повысился! Теперь он равен {typelvl++}");
+            }
+        }
+        public class Type2
+        {
+            public event TypeDeleg TypeEvent;
+            public int typelvl = 2;
+            public void LvlUp()
+            {
+                TypeEvent.Invoke($"Уровень Type2 повысился! Теперь он равен {typelvl++}");
+            }
+        }
+        public class Type3
+        {
+            public event TypeDeleg TypeEvent;
+            public int typelvl = 4;
+            public void LvlUp()
+            {
+                TypeEvent.Invoke($"Уровень Type3 повысился! Теперь он равен {typelvl++}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using static Laba9.Laba9;
+
+namespace Laba9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User(10, 2);
+            Type1 obj1 = new Type1();
+            Type2 obj2 = new Type2();
+            Type3 obj3 = new Type3();
+
+            user.Move += message => Console.WriteLine(message);
+            user.Squeeze += OutputBlueMessage;
+            user.FuncMove(12);
+            user.FuncSqueeze(4);
+            user.FuncMove(-7);
+            user.FuncSqueeze(2);
+
+            Console.WriteLine();
+
+            obj1.TypeEvent += message => Console.WriteLine(message);
+            obj2.TypeEvent += OutputBlueMessage;
+            obj3.TypeEvent += message => Console.WriteLine(message);
+            obj3.TypeEvent += OutputBlueMessage;
+            obj1.LvlUp();
+            obj2.LvlUp();
+            obj2.LvlUp();
+            obj3.LvlUp();
+            obj3.LvlUp();
+
+            Console.WriteLine("\n------------------------------------------------------\n");
+
+            string str = "Эххх.....     Был у меня когда-то товарищ,           а оказался......    мычь..........";
+            Action<string> DelegAction = message => Console.WriteLine(message); // Создание делегата по шаблону
+            DelegAction("Исходная строка:\n" + str + "\n");
+            Func<string, string> DelegFunc; // Создание делегата по шаблону
+            DelegFunc = DeleteDots;
+            str = DelegFunc(str);
+            DelegFunc = ToUp;
+            str = DelegFunc(str);
+            DelegFunc = DeleteAllSpaces;
+            str = DelegFunc(str);
+            DelegFunc = AddSpaces;
+            str = DelegFunc(str);
+            DelegFunc = DeleteSpaces;
+            str = DelegFunc(str);
+            DelegAction("Строка после обработки всеми функциями: \n" + DelegFunc(str) + "\n");
+        }
+
+
+        public static string DeleteSpaces(string str)
+        {
+            string result = "";
+            bool mark = false;
+            foreach (char el in str)
+            {
+                if (el == ' ' && mark == false)
+                {
+                    result += el;
+                    mark = true;
+                }
+                else if (el != ' ')
+                {
+                    result += el;
+                    mark = false;
+                }
+            }
+            return result;
+        }
+        public static string DeleteDots(string str)
+        {
+            string result = "";
+            bool mark = false;
+            foreach (char el in str)
+            {
+                if (el == '.' && mark == false)
+                {
+                    result += el;
+                    mark = true;
+                }
+                else if (el != '.')
+                {
+                    result += el;
+                    mark = false;
+                }
+            }
+            return result;
+        }
+        public static string ToUp(string str) 
+        { 
+            return str.ToUpper();
+        }
+        public static string DeleteAllSpaces(string str)
+        {
+            string result = "";
+            foreach (char el in str)
+            {
+                if (el == ' ')
+                {
+                    continue;
+                }
+                result += el;
+            }
+            return result;
+        }
+        public static string AddSpaces(string str)
+        {
+            string result = "";
+            foreach (char el in str)
+            {
+                if (el == ' ')
+                {
+                    result += el;
+                }
+                result += el;
+            }
+            return result;
+        }
+    }
+
+
+}
